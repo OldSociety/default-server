2150d00fbe816d14af962abac613efc2f6e6b08c
OldSociety <headmistrust@outlook.com>
2022-04-18 20:50:13 -0700

    default server start

---
+++ knexfile.js
@@ -0,0 +1,59 @@
+if (process.env.USER) require("dotenv").config();
+require('dotenv').config({path: 'path_to_env_file'});
+
+const path = require("path");
+
+const {
+  DATABASE_URL
+} = process.env;
+
+module.exports = {
+  heroku: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      directory: "knex_migrations",
+      tableName: "knex_migrations"
+    }
+  },
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
---
+++ src/races/races.controller.js
@@ -0,0 +1,28 @@
+// const races = require('../db/01-races.json')
+// const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+// async function read(req, res, next) {
+//   res.json({ data: res.locals.race });
+// }
+
+// async function raceExists(req, res, next) {
+//   const { raceId } = req.params
+//   const foundRace = races.find((race) => race.id === Number(raceId))
+//   if (foundRace) {
+//     res.locals.race = foundRace
+//     return next()
+//   }
+//   next({
+//     status: 404,
+//     message: `Paste id not found: ${raceId}`,
+//   })
+// }
+
+// async function list(req, res, next) {
+//   res.json({ data: races })
+// }
+
+// module.exports = {
+//   read: [asyncErrorBoundary(raceExists), read],
+//   list,
+// }
